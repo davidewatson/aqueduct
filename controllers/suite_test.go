68f49c71413994091e46368fa968868c725c3755
David Watson <dwat@fb.com>
2021-10-27 08:48:01 -0700

    Define NamespaceSecret CRD. We can use this in a pinch, instead of watching core types.

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -65,6 +65,9 @@ var _ = BeforeSuite(func() {
 	err = aqueductv1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
+	err = aqueductv1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
 	//+kubebuilder:scaffold:scheme
 
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
